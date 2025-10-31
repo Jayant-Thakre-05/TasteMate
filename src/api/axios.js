8a76a1a2638c565c63fcf62452f95aecc907b26b
Jayant-Thakre-05 <jayantthakre@gmail.com>
2025-10-31 20:47:39 +0530

    jnjk

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,6 +4,9 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'https://tastemate05.netlify.app/',
   withCredentials: true, // only if backend uses cookies; you can set false otherwise
+  "content/headers":{
+    
+  }
 });
 
 export default api;
